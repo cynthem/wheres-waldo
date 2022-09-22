cf4c864107916d9d45ad1a3816da95d473f1406b
doozles411 <cindy.phelps01@gmail.com>
2022-09-22 14:05:23 -0700

    Upload fonts

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Game = () => {
     return (
         <div className='game'>
-            Game
+            
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import title from '../assets/wheresWaldo.jpg';
+import title from '../assets/title.png';
 import waldo from '../assets/waldo.png';
 import odlaw from '../assets/odlaw.png';
 import whitebeard from '../assets/whitebeard.png';
@@ -7,13 +7,15 @@ import whitebeard from '../assets/whitebeard.png';
 const Header = () => {
     return (
         <div className='header'>
-            <div className='header-characters'>
-                <img src={waldo} alt="Waldo" />
-                <img src={odlaw} alt="Odlaw" />
-                <img src={whitebeard} alt="Whitebeard" />
+            <div className='header-container'>
+                <div className='header-characters'>
+                    <img src={waldo} alt="Waldo" />
+                    <img src={odlaw} alt="Odlaw" />
+                    <img src={whitebeard} alt="Whitebeard" />
+                    <div className='header-timer'>00m:00s</div>
+                </div>
+                <img className='header-title' src={title} alt="Where's Waldo?" />
             </div>
-            <div className='header-timer'>00m:00s</div>
-            <img className='header-title' src={title} alt="Where's Waldo?" />
             <button className='header-leaderboard'>Leaderboard</button>
         </div>
     )
