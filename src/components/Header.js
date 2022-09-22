1785940979c1f7d657d300ab77f7221c2fcf8088
doozles411 <cindy.phelps01@gmail.com>
2022-09-22 13:42:24 -0700

    Render header images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import title from '../assets/wheresWaldo.jpg';
+import waldo from '../assets/waldo.png';
+import odlaw from '../assets/odlaw.png';
+import whitebeard from '../assets/whitebeard.png';
 
 const Header = () => {
     return (
         <div className='header'>
-            Header
+            <div className='header-characters'>
+                <img src={waldo} alt="Waldo" />
+                <img src={odlaw} alt="Odlaw" />
+                <img src={whitebeard} alt="Whitebeard" />
+            </div>
+            <div className='header-timer'>00m:00s</div>
+            <img className='header-title' src={title} alt="Where's Waldo?" />
+            <button className='header-leaderboard'>Leaderboard</button>
         </div>
     )
 }
