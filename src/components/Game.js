b040d00d15590242ec2dc34afd8b77ad69d84678
doozles411 <cindy.phelps01@gmail.com>
2022-09-22 16:36:05 -0700

    Render pre-game text and start button

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Game = () => {
     return (
         <div className='game'>
-            
+            <div className='game-pre'>
+                <h2>Find Waldo, Odlaw, and Whitebeard in the shortest amount of time!</h2>
+                <button className='start-btn'>Start</button>
+            </div>
+            <div className='game-start'></div>
         </div>
     )
 }
