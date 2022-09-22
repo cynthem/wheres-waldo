ae3e1a5f8ad34ad599d32911edfa799ec36200e8
doozles411 <cindy.phelps01@gmail.com>
2022-09-22 16:59:22 -0700

    Render pre-game text and start button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Game from './components/Game';
 
 function App() {
+  const [isStarted, setIsStarted] = useState(false);
+
+  const handleStart = () => {
+    setIsStarted(true);
+  }
+
   return (
     <div className="App">
       <Header />
---
+++ src/components/PreGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PreGame = () => {
+    return (
+        <div className='pre-game'>
+            <h2>Find Waldo, Odlaw, and Whitebeard in the shortest amount of time!</h2>
+            <button className='start-btn'>Begin Game</button>
+        </div>
+    )
+}
+
+export default PreGame;
