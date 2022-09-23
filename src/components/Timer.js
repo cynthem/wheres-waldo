1959de789c856ce2715946f95a31adc4236ae27a
doozles411 <cindy.phelps01@gmail.com>
2022-09-23 13:27:02 -0700

    Render timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Timer = ({ time }) => {
-    
+const Timer = ({ timerStarted, time }) => {
+    const formatTime = (time) => {
+        const seconds = `0${(time % 60)}`.slice(-2);
+        const findMin = `${Math.floor(time / 60)}`;
+        const minutes = `0${(findMin)}`.slice(-2);
+        return `${minutes}m:${seconds}s`;
+    }
 
     return (
-        <div className='header-timer'></div>
+        <>
+            {!timerStarted &&
+                <div className='header-timer'>00m:00s</div>
+            }
+            {timerStarted &&
+                <div className='header-timer'>{formatTime(time)}</div>
+            }
+        </>
     )
 }
 
