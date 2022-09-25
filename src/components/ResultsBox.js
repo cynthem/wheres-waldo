dc766169b0382f157afdd451f0d96edc575f9f7a
doozles411 <cindy.phelps01@gmail.com>
2022-09-25 15:14:36 -0700

    Render ResultsBox

--- src/components/ResultsBox.js
+++ src/components/ResultsBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ResultsBox = () => {
     return (
         <div className='resultsbox'>
-            results
+            You found Whitebeard!
         </div>
     )
 }
