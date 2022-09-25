a7e3d40b884f083a259d9c4953ddd711e7f03c79
doozles411 <cindy.phelps01@gmail.com>
2022-09-25 15:26:47 -0700

    Style ResultsBox

--- src/components/ResultsBox.js
+++ src/components/ResultsBox.js
@@ -3,7 +3,8 @@ import React from 'react';
 const ResultsBox = () => {
     return (
         <div className='resultsbox'>
-            You found Whitebeard!
+            <p className='text-top'>Nothing there.</p>
+            <p className='text-bottom'>Better luck next time!</p>
         </div>
     )
 }
