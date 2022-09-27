d8780288e9793712dfb0e1db15ca481bbfb7512a
doozles411 <cindy.phelps01@gmail.com>
2022-09-27 11:27:08 -0700

    Format user times

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,6 +2,13 @@ import React from 'react';
 import uniqid from 'uniqid';
 
 const Leaderboard = ({ handleOpenLeader, highScores }) => {
+    const formatTimes = (timing) => {
+        const seconds = `0${(timing % 60)}`.slice(-2);
+        const findMin = `${Math.floor(timing / 60)}`;
+        const minutes = `0${(findMin)}`.slice(-2);
+        return `${minutes}m:${seconds}s`;
+    }
+
     return (
         <div className='leaderboard'>
             <h2 className='leaderboard-title'>LEADERBOARD</h2>
@@ -22,7 +29,7 @@ const Leaderboard = ({ handleOpenLeader, highScores }) => {
                         <div className='leader-result' key={uniqid()}>
                             <p className='leader-place'>{index + 1}</p>
                             <p className='leader-name'>{score.player}</p>
-                            <p className='leader-time'>{score.timing}</p>
+                            <p className='leader-time'>{formatTimes(score.timing)}</p>
                         </div>
                     )}
                 </div>
