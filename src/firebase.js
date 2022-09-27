4d7962f3c4dd4fa340abca6c34e654448034712c
doozles411 <cindy.phelps01@gmail.com>
2022-09-26 17:04:54 -0700

    Update firebase imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
-import * as firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCqs8esJ_xGcwmaVhdGrQndBxtTMPFYujc",
   authDomain: "where-s-waldo-2dd2f.firebaseapp.com",
   projectId: "where-s-waldo-2dd2f",
+  databaseURL: "https://where-s-waldo-2dd2f-default-rtdb.firebaseio.com/",
   storageBucket: "where-s-waldo-2dd2f.appspot.com",
   messagingSenderId: "70988581152",
   appId: "1:70988581152:web:1368f798750ead73116f45"
